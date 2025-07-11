dfebf3bb1c1eaf758270de1f5e378f6c99ddc6d2
sangarix <denya.garkusha.12@gmail.com>
2025-07-11 18:05:33 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-import { openBar } from './js/Header';
-import { cookiePolicy } from './js/cookiesPolicy';
+import { openBar } from './js/header.js';
+import { cookiePolicy } from './js/cookiesPolicy.js';
 const swiper = new Swiper('.swiper', {
   loop: true,
   spaceBetween: -10,
